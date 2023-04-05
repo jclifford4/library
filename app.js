83897b08c2f55bba3cb208273a9be3d56c900f33
Joseph Clifford <92230691+jclifford4@users.noreply.github.com>
2023-04-05 04:46:17 -0700

    Sync form read and book read button colors

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const addButton = document.querySelector('.add-button');
 const bookList = document.querySelector('.book-list');
 const popUpForm = document.querySelector('.form-popup');
 const popUpSubmit = document.querySelector('.submit');
+
 popUpForm.style.display = 'none';
 let allActiveBookDivs;
 let books;
 const html = document.querySelector('html');
 let deleteButtons;
+let readButtons;
 
-function Book(title, author, genre, pages, read) {
+function Book(title, author, genre, pages, read = false) {
   this.title = title;
   this.author = author;
   this.genre = genre;
@@ -18,10 +20,43 @@ function Book(title, author, genre, pages, read) {
   this.read = read;
 }
 
+function getBookFromTitle(form) {
+  return activeBookLibrary.find((book) => {
+    return book.title === form.childNodes[1].childNodes[1].value;
+  });
+}
+
 // Always update the DOM element classes.
 html.addEventListener('mouseover', (e) => {
   allActiveBookDivs = document.querySelectorAll('.active-book'); // get all displayed books
   deleteButtons = document.querySelectorAll('.delete-button'); // get all delete buttons
+  readButtons = document.querySelectorAll('.read');
+
+  readButtons.forEach((button) => {
+    if (!button.classList.contains('active')) {
+      if (!button.parentElement.classList.contains('active-book')) {
+        button.classList.add('off');
+        button.style.backgroundColor = 'red';
+      }
+
+      button.classList.add('active');
+      button.addEventListener('click', () => {
+        let book = findBookInArray(button);
+        if (button.parentNode.classList.contains('active-book'))
+          book.read = !book.read;
+
+        if (button.classList.contains('on')) {
+          button.classList.remove('on');
+          button.classList.add('off');
+          button.style.backgroundColor = 'red';
+        } else if (button.classList.contains('off')) {
+          button.classList.remove('off');
+          button.classList.add('on');
+          button.style.backgroundColor = 'green';
+        }
+      });
+    }
+  });
 
   // For every delete button on the DOM
   deleteButtons.forEach((button) => {
@@ -44,10 +79,13 @@ popUpSubmit.addEventListener('click', function (event) {
 popUpSubmit.addEventListener('click', () => {
   const currentEmptyBook = createBook(); // Create an empty book object.
   currentBookData = getBookInfoFromUserInput(currentEmptyBook);
-  addBookToLibrary(currentBookData);
-  closeForm();
-  populateBookToBookList(currentBookData); // Visually add the book to the list.
-  // Querey all displayed books.
+  if (!currentBookData.hasOwnProperty('title')) delete currentBookData;
+  else {
+    addBookToLibrary(currentBookData);
+    closeForm();
+    populateBookToBookList(currentBookData); // Visually add the book to the list.
+    // Querey all displayed books.
+  }
 });
 
 // Open popup form on click from add button
@@ -98,22 +136,45 @@ function createBookDiv(currentBookData) {
 // Get data from form inputs, return it.
 function getBookInfoFromUserInput(book) {
   let bookInfoInputs = document.querySelectorAll('input');
-  console.log(bookInfoInputs);
   book.title = bookInfoInputs[0].value;
   book.author = bookInfoInputs[1].value;
   book.pages = bookInfoInputs[2].value;
   book.genre = bookInfoInputs[3].value;
   book.read = bookInfoInputs[4].value;
+  if (bookInfoInputs[4].classList.contains('on')) {
+    book.read = true;
+  } else if (bookInfoInputs[4].classList.contains('off')) {
+    book.read = false;
+  }
+
+  for (const [key, value] of Object.entries(book)) {
+    if (value === '') {
+      closeForm();
+      return {};
+    }
+  }
+
+  console.log(bookInfoInputs);
   return book;
 }
 
 // Create and Fill in the inner data for the current book.
 function createBookDataDivs(bookDiv, currentBookData) {
   for (let i = 0; i < Object.keys(currentBookData).length; i++) {
-    const currentBookDataDiv = document.createElement('div');
-    currentBookDataDiv.classList.add(`data-div${i}`);
-    currentBookDataDiv.textContent = `${Object.values(currentBookData)[i]}`;
-    bookDiv.appendChild(currentBookDataDiv);
+    if (i === 4) {
+      const rd = document.createElement('button');
+      rd.classList.add(`data-div${i}`);
+      rd.classList.add('read');
+      let readWrap = document.querySelector('.read-wrap');
+      rd.style.backgroundColor = `${readWrap.childNodes[3].style.backgroundColor}`;
+      rd.classList.add(`${readWrap.childNodes[3].classList[2]}`);
+      bookDiv.appendChild(rd);
+    } else {
+      const currentBookDataDiv = document.createElement('div');
+      currentBookDataDiv.classList.add(`data-div${i}`);
+      currentBookDataDiv.textContent = `${Object.values(currentBookData)[i]}`;
+      bookDiv.appendChild(currentBookDataDiv);
+    }
   }
 
   appendDeleteBookButtonToCurrentBookDiv(bookDiv);
